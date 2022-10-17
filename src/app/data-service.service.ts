5a46653e717ef211246ee1a299f4fec4c163d711
trung <dinhtrung2515@gmail.com>
2022-10-17 15:50:28 +0700

    demo

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -7,32 +7,125 @@ export class DataServiceService {
 
   constructor() { }
   data:any = [
-    {
-      "id": 1,
-      "question": "cố dâu... tuổi",
-      "answer": [
-        "1","2","8","10"
-      ],
-      "Correct answer": "8",
-      "inBook":"Câu 1"
-    },
+    // {
+    //   "id": 1,
+    //   "question": 'He has been very interested in doing research on ............. since he was at high school.',
+    //   "answer": [
+    //     "biology ","biological","biologist","bjologically "
+    //   ],
+    //   "correctAnswer": "8",
+    //   "inBook":"Câu 1",
+    //   "trick":"vtv "
+    // },
     {
       "id": 2,
-      "question": "Một cây làm chẳng...non",
+      "question": 'Are you sure that boys are more ............ than girls?',
       "answer": [
-        "nên","sai","hư","hao"
+        "act ","active","action","activity"
       ],
-      "Correct answer": "nên",
-      "inBook":"Câu 2"
+      "correctAnswer": "active",
+      "inBook":"Câu 2",
+      "trick":""
     },
+    // {
+    //   "id": 3,
+    //   "question": 'it iS generally believed that “Men make ..... house and women make .... home',
+    //   "answer": [
+    //     "no article/ no article ","a/an","the/ the","an/ the"
+    //   ],
+    //   "correctAnswer": "",
+    //   "inBook":"Câu 3",
+    //   "trick":""
+    // },
     {
-      "id": 3,
-      "question": "bên trên tầng lầu - ....",
+      "id": 4,
+      "question": 'Most doctors and nurses have to work on a ............ once or twice a week at the hospital.',
       "answer": [
-        "Phan Mạnh Quỳnh","Tiên Tiên","Tăng Duy Tân","Trung Quân"
+        "solution  ","night shift","household chores","special dishes"
       ],
-      "Correct answer": "Tăng Duy Tân",
-      "inBook":"Câu 3"
+      "correctAnswer": "night shift",
+      "inBook":"Câu 4",
+      "trick":""
+    }
+    ,{
+      "id": 5,
+      "question": 'We enjoy....... time together in the evening when the family members gather in the living room After a day of working hard',
+      "answer": [
+        "spending","caring","taking","doing"
+      ],
+      "correctAnswer": "spending",
+      "inBook":"Câu 5",
+      "trick":" enjoy + V + ing"
+    }
+    ,{
+      "id": 6,
+      "question": "It is parents' duty and responsibility to..... hands to take care of their chỉldren and give them a happy home",
+      "answer": [
+        "shake","hold","join","take"
+      ],
+      "correctAnswer": "join",
+      "inBook":"Câu 6",
+      "trick":" jion hands = kết hợp chặt chẽ"
+    }
+    ,{
+      "id": 7,
+      "question": "He is a boy. He is often kind and helpful to every classmate.",
+      "answer": [
+        "frank","lovely","obedient","caring"
+      ],
+      "correctAnswer": "caring",
+      "inBook":"Câu 7",
+      "trick":" caring = Chu đáo"
+    }
+    ,{
+      "id": 8,
+      "question": "Doctors are supposed to ...... responsibility for human life",
+      "answer": [
+        "do","take","rush","join"
+      ],
+      "correctAnswer": "take",
+      "inBook":"Câu 8",
+      "trick":" take responsibility  = chiệu trách nhiệm"
+    }
+    ,{
+      "id": 9,
+      "question": "............ sure that you follow the instructions carefully.",
+      "answer": [
+        "Believe","Try","do","make"
+      ],
+      "correctAnswer": "make",
+      "inBook":"Câu 9",
+      "trick":""
+    }
+    ,{
+      "id": 10,
+      "question": "Sometimes Mr. Pike has to work very late....... night to do some important experiments.",
+      "answer": [
+        "in","at","for","on"
+      ],
+      "correctAnswer": "at",
+      "inBook":"Câu 10",
+      "trick":""
+    }
+    ,{
+      "id": 11,
+      "question": "Peter was....... a hurry to go so he did not stop to greet me.",
+      "answer": [
+        "in","on","with","over"
+      ],
+      "correctAnswer": "in",
+      "inBook":"Câu 11",
+      "trick":"in a hunry = đang vội"
+    }
+    ,{
+      "id": 12,
+      "question": "it... it is parents' responsibility to take good care of their children.",
+      "answer": [
+        "commonly says that","commonly to be said that","is commonly said that","is commonly saying"
+      ],
+      "correctAnswer": "is commonly said that",
+      "inBook":"Câu 12",
+      "trick":"is commonly said that = người ta thường nói rằng"
     }
   ]
       
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -12,26 +12,54 @@ export class PagesComponent implements OnInit {
   constructor( public apiService:MainService,public question:DataServiceService) { }
 
   ngOnInit(): void {
-  //  this.getData()
+
   this.dataQuestion = this.question.data
 
   
   }
   dataQuestion:any = []
-  // getData(){
-  //   this.apiService.get('http://localhost:3000/question').subscribe((data:any) => {
-  //     console.log(data);
-  //     this.dataQuestion = data
-  //   })
-  // }
+
   listAnswer:any =[]
-  answerQues(id:number,answer:any){
-    console.log(id,answer);
-    this.listAnswer.reduce((pre:any , cur:any) => {
-      // if(pre[id]){
-      //   pre.id = 
-      // }
-    },{})
+  poin:number = 0
+  showTrick: boolean = false
+  answerQues(myId:number,myAnswer:any){
+    console.log(myId,myAnswer);
+    let check = this.listAnswer.filter((e:any) => e.id === myId) 
+  
+    if(check.length == 0){
+
+      this.listAnswer = [...this.listAnswer,{id: myId , answer:myAnswer}]
+    }else{
+ 
+      let index = this.listAnswer.findIndex((e:any) => e.id == check[0].id)
+      this.listAnswer.splice(index,1,{id: myId , answer: myAnswer})
+    }
+    console.log('list answer',this.listAnswer);
     
   }
+  title:any = ''
+  mark(){
+    let countTrueAnswer = 0
+    let data = [...this.dataQuestion]
+    this.listAnswer.forEach((e:any) => {
+      let creQues:any = data.filter((q:any) => q.id == e.id)
+      e.answer == creQues[0].correctAnswer? countTrueAnswer ++ : ''
+    });
+
+    this.poin = (countTrueAnswer/this.dataQuestion.length)*10
+    this.showTrick = true
+    this.title = `${this.poin} điểm`
+    
+  }
+  randomQues(){
+    this.listAnswer = []
+    this.showTrick = false
+  
+  
+    this.dataQuestion.sort( (a:any, b:any) => {      
+      return  0.5  - Math.random( )
+    } )
+  }
+
+
 }
