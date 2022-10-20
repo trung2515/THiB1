984076208a6c3f9e96677b5f9b9c4d728465b823
trung <dinhtrung2515@gmail.com>
2022-10-20 08:27:06 +0700

    add ques

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -159,13 +159,13 @@ export class DataServiceService {
     }
     ,{
       "id": 16,
-      "question": "They............. for 3 hourS when the storm suddenly broke.",
+      "question": "They............. for 3 hours when the storm suddenly broke.",
       "answer": [
         "had been running","have been running","are running","will be running"
       ],
       "correctAnswer": "had been running",
       "inBook":"Câu 16",
-      "trick":"trước when là 1 câu thì quá khứ hoàn thành"
+      "trick":""
     }
     ,{
       "id": 17,
@@ -199,7 +199,7 @@ export class DataServiceService {
     }
     ,{
       "id": 20,
-      "question": "......get tired ofanswering the same questions every day?",
+      "question": "...........get tired ofanswering the same questions every day?",
       "answer": [
         "Have you ever"," Had you ever","Do you ever","Are you ever"
       ],
@@ -219,7 +219,7 @@ export class DataServiceService {
     }
     ,{
       "id": 22,
-      "question": "I.......... there once a long time ago and I .. back since",
+      "question": "I.......... there once a long time ago and I ........ back since",
       "answer": [
         "went/ have not been","go/ am not","have gone/was","was going/had not been"
       ],
@@ -290,7 +290,7 @@ export class DataServiceService {
     }
     ,{
       "id": 29,
-      "question": "According to the boss, John is the most....of executive secretary",
+      "question": "According to the boss, John is the most.........of executive secretary",
       "answer": [
         "supportive","caring","suitable","comfortable"
       ],
@@ -300,7 +300,7 @@ export class DataServiceService {
     }
     ,{
       "id": 30,
-      "question": "She gọt up late and RUSH to the bus stop.",
+      "question": "She got up late and RUSH to the bus stop.",
       "answer": [
         "came into","went leisurely","went quickly","dropped by"
       ],
@@ -370,7 +370,7 @@ export class DataServiceService {
     }
     ,{
       "id": 37,
-      "question": "We are not allowed.....jeans at schools",
+      "question": "We are not allowed..........jeans at schools",
       "answer": [
         "wear","to wear","wearing","worn"
       ],
@@ -388,6 +388,538 @@ export class DataServiceService {
       "inBook":"Câu 40",
       "trick":""
     }
+    ,{
+      "id": 41,
+      "question": "I love............ flms but I seldom find time to go to the cinema.",
+      "answer": [
+        "see","saw","seen","seeing"
+      ],
+      "correctAnswer": "seeing",
+      "inBook":"Câu 41",
+      "trick":""
+    }
+    ,{
+      "id": 42,
+      "question": "In the last hundred years, traveling ........ much easier and more comfortable.",
+      "answer": [
+        "becomes","has become","became","will become"
+      ],
+      "correctAnswer": "has become",
+      "inBook":"Câu 42",
+      "trick":""
+    }
+    ,{
+      "id": 43,
+      "question": "In the nineteenth century,it ......two or three months to cross Norrth America by covered wagon.",
+      "answer": [
+        "took","had taken","has taken","was taking"
+      ],
+      "correctAnswer": "took",
+      "inBook":"Câu 43",
+      "trick":""
+    }
+    ,{
+      "id": 44,
+      "question": "In the past, the trip.... very rough and dangerous, but things ....... a great deal in the last hundred and fifty years",
+      "answer": [
+        "was/ have changed","is/ change","had been/ will change","has been/ changed"
+      ],
+      "correctAnswer": "was/ have changed",
+      "inBook":"Câu 44",
+      "trick":""
+    }
+    ,{
+      "id": 45,
+      "question": " Now you........ from New York to Los Angeles in a matter of hours",
+      "answer": [
+        "are flying","would fly","will fly","can fly"
+      ],
+      "correctAnswer": "can fly",
+      "inBook":"Câu 45",
+      "trick":""
+    }
+    ,{
+      "id": 46,
+      "question": " When Carol........... last night,I........... my favorite show on television.",
+      "answer": [
+        "was calling/ watched","called/ have watched","called/ was watching","had called/ watched"
+      ],
+      "correctAnswer": "called/ was watching",
+      "inBook":"Câu 46",
+      "trick":""
+    }
+    ,{
+      "id": 47,
+      "question": "By this time next summer, you.......your studies.",
+      "answer": [
+        "completes","will complete","are completing","will have completed"
+      ],
+      "correctAnswer": "will have completed",
+      "inBook":"Câu 47",
+      "trick":""
+    }
+    ,{
+      "id": 48,
+      "question": " Right now, Jim ........  the newspaper, and Kathy........... dinner.",
+      "answer": [
+        "reads/ has cooked","is reading/ is cooking","has read/ was cooking","read/ will be cooking"
+      ],
+      "correctAnswer": "is reading/ is cooking",
+      "inBook":"Câu 48",
+      "trick":""
+    }
+    ,{
+      "id": 49,
+      "question": "Last night at this time, they.....the same thing. She............ and he .......the newspaper.",
+      "answer": [
+        "are doing/ is cooking/ is reading.","were doing/ was cooking/ was reading","was doing/ has cooked/ is reading,","had done/ was cooking/ read"
+      ],
+      "correctAnswer": "were doing/ was cooking/ was reading",
+      "inBook":"Câu 49",
+      "trick":""
+    }
+    ,{
+      "id": 50,
+      "question": "When I............ home last night, I.....that Jane.......... a beautiful candlelight dinner.",
+      "answer": [
+        "had arrived/ discovered/ prepared","was arriving/ had discovered/ was preparing","have arrived/ was discovering/ had prepared","arrived/ descovered/ was preparing"
+      ],
+      "correctAnswer": "arrived/ descovered/ was preparing",
+      "inBook":"Câu 50",
+      "trick":""
+    }
+    ,{
+      "id": 51,
+      "question": "He was looking at his parents........waiting for recommendations.",
+      "answer": [
+        "obey","obedience","obedient","obedienty"
+      ],
+      "correctAnswer": "obedienty",
+      "inBook":"Câu 51",
+      "trick":""
+    }
+    ,{
+      "id": 52,
+      "question": "The interview with parents showed that the vast majority were............ Of teachers.",
+      "answer": [
+        "support","supportive","supporter","supporting"
+      ],
+      "correctAnswer": "supportive",
+      "inBook":"Câu 52",
+      "trick":""
+    }
+    // ,{
+    //   "id": 53,
+    //   "question": "Billy has been serious ,and he was taken to ............. hospital yesterday.",
+    //   "answer": [
+    //     "a","an","the","no article"
+    //   ],
+    //   "correctAnswer": "supportive",
+    //   "inBook":"Câu 53",
+    //   "trick":""
+    // }
+    ,{
+      "id": 54,
+      "question": "My husband and I both go out to work so she share the........",
+      "answer": [
+        "happiness","household chores","responsibility","employment"
+      ],
+      "correctAnswer": "household chores",
+      "inBook":"Câu 54",
+      "trick":""
+    }
+    ,{
+      "id": 55,
+      "question": "you should not burn ..... You had better dig a hole and bury it",
+      "answer": [
+        "dishes","lab","garbage","shift"
+      ],
+      "correctAnswer": "garbage",
+      "inBook":"Câu 55",
+      "trick":""
+    }
+    ,{
+      "id": 56,
+      "question": "He was very respectful at home and ...... to his parents",
+      "answer": [
+        "responsible","caring","obedient","lovely"
+      ],
+      "correctAnswer": "obedient",
+      "inBook":"Câu 56",
+      "trick":""
+    }
+    ,{
+      "id": 57,
+      "question": "One of vietnamese traditions is a belief in ...... families and in preserving their cultures",
+      "answer": [
+        "wealthy","secure","safe","close-knit"
+      ],
+      "correctAnswer": "close-knit",
+      "inBook":"Câu 57",
+      "trick":""
+    }
+    ,{
+      "id": 58,
+      "question": "He was ........... when I had those problems and said whatever I did he would stand by me",
+      "answer": [
+        "supportive","exciting","busy","dull"
+      ],
+      "correctAnswer": "supportive",
+      "inBook":"Câu 58",
+      "trick":""
+    }
+    ,{
+      "id": 59,
+      "question": "He was determined to give family  a secure and solid ...........",
+      "answer": [
+        "base","floor","basement","ground"
+      ],
+      "correctAnswer": "base",
+      "inBook":"Câu 59",
+      "trick":""
+    }
+    // ,{
+    //   "id": 60,
+    //   "question": "It is hard Work looking ....... three children all day",
+    //   "answer": [
+    //     "after","up","to","through"
+    //   ],
+    //   "correctAnswer": "to",
+    //   "inBook":"Câu 60",
+    //   "trick":""
+    // }
+    ,{
+      "id": 61,
+      "question": "When my mother is busy preparing dinner, my father often gives a hand ...........tidying the living room",
+      "answer": [
+        "on","with","for","about"
+      ],
+      "correctAnswer": "with",
+      "inBook":"Câu 61",
+      "trick":"gives a hand  + with"
+    }
+    ,{
+      "id": 62,
+      "question": "I put your keys in the drawer........ they Cannot get lost",
+      "answer": [
+        "because","let alone","instead of","so that"
+      ],
+      "correctAnswer": "so that",
+      "inBook":"Câu 62",
+      "trick":""
+    }
+    ,{
+      "id": 63,
+      "question": "alex is busy ......... for his exams",
+      "answer": [
+        "to study","studied","studying","studies"
+      ],
+      "correctAnswer": "studied",
+      "inBook":"Câu 63",
+      "trick":""
+    }
+    // ,{
+    //   "id": 64,
+    //   "question": "She is never willing .. any personal question.",
+    //   "answer": [
+    //     "answer","to answer","answering","answered"
+    //   ],
+    //   "correctAnswer": "",
+    //   "inBook":"Câu 64",
+    //   "trick":""
+    // }
+    // ,{
+    //   "id": 65,
+    //   "question": "...a doctor, you have †o meet some certain Tequirements of the medical college.",
+    //   "answer": [
+    //     "To become","Become","Becoming","Become"
+    //   ],
+    //   "correctAnswer": "",
+    //   "inBook":"Câu 65",
+    //   "trick":""
+    // }
+    ,{
+      "id": 66,
+      "question": "Sam......... to change a light bulb when he...... and .....",
+      "answer": [
+        "Was trying/ Slipped/ fell","tried/ was sleeping / falling","had been trying/ slipped was falling","has tried / slips / falls"
+      ],
+      "correctAnswer": "Was trying/ Slipped/ fell",
+      "inBook":"Câu 66",
+      "trick":""
+    }
+    ,{
+      "id": 67,
+      "question": "Every day I ...... up at 6 o'clock, ....... breakfast at 7 o'clock and ...... for work at 8 o'clock",
+      "answer": [
+        "get/ eat/ leave","have got/ eating / leaving","got/ ate/ left","will get/ have eaten/ left"
+      ],
+      "correctAnswer": "get/ eat/ leave",
+      "inBook":"Câu 67",
+      "trick":""
+    }
+    ,{
+      "id": 68,
+      "question": "Yesterday, I ............. for work late because I ......... to set my alarm",
+      "answer": [
+        "had left/ forgot","was leaving/ was forgetting","left/ had forgot","had been leaving/ would forget"
+      ],
+      "correctAnswer": "left/ had forgot",
+      "inBook":"Câu 68",
+      "trick":""
+    }
+    ,{
+      "id": 69,
+      "question": "By the time we............ to the train station, Susan.........for us for more than two hours",
+      "answer": [
+        "win get/ has been waiting"," got/ was waiting ","got/ had been waiting","get/ will wait"
+      ],
+      "correctAnswer": "got/ had been waiting",
+      "inBook":"Câu 69",
+      "trick":""
+    }
+    ,{
+      "id": 70,
+      "question": "I............ for this company for more than thirtty years, and I intend to stay here until I..........next year",
+      "answer": [
+        "am working/ will retire"," am going to work/ am retiring ","work/ am going to retire","have been working/ retire"
+      ],
+      "correctAnswer": "have been working/ retire",
+      "inBook":"Câu 70",
+      "trick":""
+    }
+    ,{
+      "id": 71,
+      "question": "My mother is always the first............. up and the last ..........to bed",
+      "answer": [
+        "getting/ going","to get/ going","getting/ to go","to get/ to go"
+      ],
+      "correctAnswer": "to get/ to go",
+      "inBook":"Câu 71",
+      "trick":""
+    }
+    ,{
+      "id": 72,
+      "question": "I............ you last night after dinner, but you.......there. Where ......you?",
+      "answer": [
+        "was calling/ are not/ are","called/ were not/ were","had called/ had not been/ were","called/ have not been/ are"
+      ],
+      "correctAnswer": "called/ were not/ were",
+      "inBook":"Câu 72",
+      "trick":""
+    }
+    ,{
+      "id": 73,
+      "question": "The Titanic............ the Atlantic when it........ an iceberg",
+      "answer": [
+        "was crossing/ struck","had crossed/ was striking","crossed/ had struck","is crossing/ strikes"
+      ],
+      "correctAnswer": "is crossing/ strikes",
+      "inBook":"Câu 73",
+      "trick":""
+    }
+    ,{
+      "id": 74,
+      "question": " ...... to come over for dinner tonight?",
+      "answer": [
+        "Do you want","Are you wanting","have you wanted","will you want"
+      ],
+      "correctAnswer": "Do you want",
+      "inBook":"Câu 74",
+      "trick":""
+    }
+    ,{
+      "id": 75,
+      "question": " what .......... at this time tomorraw?",
+      "answer": [
+        "will you do","will you be doing","will you have done","will you have been doing"
+      ],
+      "correctAnswer": "will you be doing",
+      "inBook":"Câu 75",
+      "trick":""
+    }
+    ,{
+      "id": 76,
+      "question": " London is home to people of many..........cultures?",
+      "answer": [
+        "diverse","diversity","diversify","diversification"
+      ],
+      "correctAnswer": "diverse",
+      "inBook":"Câu 76",
+      "trick":""
+    }
+    ,{
+      "id": 77,
+      "question": "John cannot make a.................to get married to Mary or stay single until he can afford a house and a Car.",
+      "answer": [
+        "decide","decision","decisive","decisively"
+      ],
+      "correctAnswer": "decision",
+      "inBook":"Câu 77",
+      "trick":""
+    }
+    ,{
+      "id": 78,
+      "question": "My mother used to be a woman of great... but now she gets old and looks pale.",
+      "answer": [
+        "beauty","beautiful","beautifully","beautify"
+      ],
+      "correctAnswer": "beauty",
+      "inBook":"Câu 78",
+      "trick":""
+    }
+    ,{
+      "id": 79,
+      "question": "My father phoned me to say that he would come ................. home late.",
+      "answer": [
+        "a","an","the","no article"
+      ],
+      "correctAnswer": "no article",
+      "inBook":"Câu 79",
+      "trick":""
+    }
+    ,{
+      "id": 80,
+      "question": "At last they divorced after ten years of........... marriage.",
+      "answer": [
+        "a","an","the","no article"
+      ],
+      "correctAnswer": "no article",
+      "inBook":"Câu 80",
+      "trick":""
+    }
+    ,{
+      "id": 81,
+      "question": "A curriculum that ignores ethnic tensions, racial antagonism, cultural............. and religious differences is not relevant",
+      "answer": [
+        "diversity","Contacts","barries","levels"
+      ],
+      "correctAnswer": "diversity",
+      "inBook":"Câu 81",
+      "trick":""
+    }
+    ,{
+      "id": 82,
+      "question": "Some researchers have just.................A survey of young poeple's points of view on contractual marriage",
+      "answer": [
+        "sent","directed","managed","conducted"
+      ],
+      "correctAnswer": "conducted",
+      "inBook":"Câu 82",
+      "trick":""
+    }
+    ,{
+      "id": 83,
+      "question": "It is not easy to .............our beauty when we get older and older.",
+      "answer": [
+        "develop","maintain","gain","collect"
+      ],
+      "correctAnswer": "maintain",
+      "inBook":"Câu 83",
+      "trick":""
+    }
+    ,{
+      "id": 84,
+      "question": "Many young people have objected to...................marriage, which decided by the parents of the bride and groom",
+      "answer": [
+        "agreed","shared","contractual","sacrificed"
+      ],
+      "correctAnswer": "contractual",
+      "inBook":"Câu 84",
+      "trick":""
+    }
+    ,{
+      "id": 85,
+      "question": "All parents are.................. to at least try to behave in ways that will give their own children an important protection.",
+      "answer": [
+        "decided","supposed","followed","rejected"
+      ],
+      "correctAnswer": "supposed",
+      "inBook":"Câu 85",
+      "trick":""
+    }
+    ,{
+      "id": 86,
+      "question": "It is thought that traditional marriage....................are important basis of limiting divorce rates.",
+      "answer": [
+        "appearances","records","responses","values"
+      ],
+      "correctAnswer": "values",
+      "inBook":"Câu 86",
+      "trick":""
+    }
+    ,{
+      "id": 87,
+      "question": "Affected by the Western cultures, Vietnamese young people's attitudes....................... love and marriage have dramatically changed",
+      "answer": [
+        "for","with","through","towards"
+      ],
+      "correctAnswer": "towards",
+      "inBook":"Câu 87",
+      "trick":""
+    }
+    ,{
+      "id": 88,
+      "question": "Sometimes she does not agree......................... her husband about child's rearing but they soon fnd the solutions",
+      "answer": [
+        "for","on","with","of"
+      ],
+      "correctAnswer": "with",
+      "inBook":"Câu 88",
+      "trick":""
+    }
+    ,{
+      "id": 89,
+      "question": "The young are .......................more concerned with physical  attractiveness than elderly people",
+      "answer": [
+        "much","as","many","as much as"
+      ],
+      "correctAnswer": "much",
+      "inBook":"Câu 89",
+      "trick":""
+    }
+    ,{
+      "id": 90,
+      "question": "lt will take MORE OR LESS a month to prepare for  the wedding",
+      "answer": [
+        "approximately","generally","fankly","simply"
+      ],
+      "correctAnswer": "approximately",
+      "inBook":"Câu 90",
+      "trick":""
+    }
+    ,{
+      "id": 91,
+      "question": "The number of the participants in the survey..............250 students for Oxford University",
+      "answer": [
+        "are","was","were","have been"
+      ],
+      "correctAnswer": "was",
+      "inBook":"Câu 91",
+      "trick":""
+    }
+    ,{
+      "id": 92,
+      "question": "the grass was yellow becasue it ....................... all summer",
+      "answer": [
+        "does not rain","has not rained","had not rained","would not rained"
+      ],
+      "correctAnswer": "had not rained",
+      "inBook":"Câu 92",
+      "trick":""
+    }
+    ,{
+      "id": 93,
+      "question": "many people even wonder these days........................",
+      "answer": [
+        "what marriage is","what is marriage","what marriage","what was marriage"
+      ],
+      "correctAnswer": "what marriage is",
+      "inBook":"Câu 93",
+      "trick":""
+    }
+
+
 
 
 
